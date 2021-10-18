6df08997168c4586010983c2ce6b54909c26ebbe
Luis Berenguer <luis01developer@gmail.com>
2021-10-18 19:50:29 +0200

    Aprendimos a recibir parámetros con NestJS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,25 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'yo soy nuevo';
+  }
+
+  @Get('/ruta/')
+  nueva() {
+    return 'soy una nueva ruta';
+  }
+
+  @Get('/products/:id')
+  getParams(@Param('id') id: any) {
+    return `prduct ${id}`;
+  }
+
+  @Get('/categories/:id/products/:productId')
+  getCategory(@Param('id') id: string, @Param('productId') productId: string) {
+    return `prduct ${productId} and category ${id}`;
   }
 }
