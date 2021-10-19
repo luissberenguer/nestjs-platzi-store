ab0c7671dd2c757f524f80c540c0f0f678d282fa
Luis Berenguer <luis01developer@gmail.com>
2021-10-19 18:50:10 +0200

    Mejoramos la validación de daotos con class-validator y con PartialType

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,15 +1,34 @@
+import {
+  IsString,
+  IsNumber,
+  IsPositive,
+  IsUrl,
+  IsNotEmpty,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsPositive()
+  @IsNumber()
   readonly price: number;
+
+  @IsNotEmpty()
+  @IsPositive()
+  @IsNumber()
   readonly stock: number;
+
+  @IsNotEmpty()
+  @IsUrl()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
