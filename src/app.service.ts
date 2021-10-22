7ed41f8a88fd3db0f97d950582f5c4e4ca28f66e
Luis Berenguer <luis01developer@gmail.com>
2021-10-22 11:10:07 +0200

    Creamos diferentes variables de entorno para 3 ambientes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,17 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config'
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    @Inject('TASKS') private tasks: any[],
+    private config: ConfigService,
+  ) {}
 
   getHello(): string {
-    const apiKey = this.configService.get('API_KEY');
-    const dbName = this.configService.get('DATABASE_NAME');
-    console.log(this.tasks);
+    const apiKey = this.config.get('API_KEY');
+    const dbName = this.config.get('DATABASE_NAME');
+    console.log(this.tasks)
     return `Hello World apiKey: ${apiKey}, dbName: ${dbName} `;
   }
 }
