080c5fa0267216f3a56c0fb127fa4f6213d038ba
Luis Berenguer <luis01developer@gmail.com>
2021-10-22 11:34:23 +0200

    Creamos un archivo de configuración de variables de entorno para no equivocarnos en el tipado

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,19 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('TASKS') private tasks: any[],
-    private config: ConfigService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
 
   getHello(): string {
-    const apiKey = this.config.get('API_KEY');
-    const dbName = this.config.get('DATABASE_NAME');
-    console.log(this.tasks)
-    return `Hello World apiKey: ${apiKey}, dbName: ${dbName} `;
+    const apiKey = this.configService.apiKey;
+    const dbName = this.configService.database.name;
+    const dbPort = this.configService.database.port;
+    console.log(this.tasks);
+    return `Hello World apiKey: ${apiKey}, dbName: ${dbName}, dbPort ${dbPort}:`;
   }
 }
