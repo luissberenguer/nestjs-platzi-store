d02c438c6c7660a88db5cf493cfdd83d57bb1a93
Luis Berenguer <luis01developer@gmail.com>
2021-10-29 11:21:08 +0200

    Buen naming en bases de datos para products

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -10,7 +10,7 @@ import {
 
 import { Customer } from './customer.entity';
 
-@Entity()
+@Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
@@ -25,18 +25,20 @@ export class User {
   role: string;
 
   @CreateDateColumn({
+    name: 'create_at',
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
   createAt: Date;
 
   @UpdateDateColumn({
+    name: 'update_at',
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
   updateAt: Date;
 
   @OneToOne(() => Customer, { nullable: true })
-  @JoinColumn()
+  @JoinColumn({name: 'customer_id'})
   customer: Customer;
 }
